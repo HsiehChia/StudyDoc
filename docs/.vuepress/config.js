ce7e725c9bbd4c396af7cd3b6e56ea73e5d6cd25
Jane <jane.xie@bundleb2b.net>
2021-05-31 17:13:54 +0800

    feat: use plugin back to top

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,9 @@ module.exports = {
     title: "Jane's blog",
     description: "Jane's personal blog",
     head,
+    plugins: {
+        '@vuepress/back-to-top': true, //返回顶部
+    },
     themeConfig: {
         lastUpdated: '上一次更新时间',
         logo: '/assets/img/logo.png',
