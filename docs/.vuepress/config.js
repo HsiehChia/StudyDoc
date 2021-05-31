fc56bb346c33c480c608c22b5f1d151f73df70a9
Jane <jane.xie@bundleb2b.net>
2021-05-31 16:56:09 +0800

    feat: split config file

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,39 +1,14 @@
+const head = require('./config/headConfig');
+const nav = require('./config/navConfig');
+const sidebar = require('./config/sidebarConfig');
 module.exports = {
     title: "Jane's blog",
     description: "Jane's personal blog",
-    head: [
-        ['link', {ref: 'icon', href: '/favicon.ico'}],
-        ['meta', {name: 'author', content: 'jane'}],
-        ['meta', {name: 'keywords', content: '前端程序员的个人博客，分享前端知识以及程序代码'}],
-    ],
+    head,
     themeConfig: {
         lastUpdated: '上一次更新时间',
         logo: '/assets/img/logo.png',
-        nav: [
-            {text: '首页', link: '/'},
-            {text: '技术分类', link: '/cate/'},
-            {text: '联系站长', link: '/contact/'},
-        ],
-        sidebar: [
-            {
-                title: '技术分类目录',
-                path: '/cate/',
-                collapsable: true,
-                sidebarDepth: 1,
-                children: [
-                  ['/cate/category', '目录结构'],
-                ]
-            },
-            {
-                title: 'HTML',
-                path: '/html/',
-                collapsable: true,
-                sidebarDepth: 1,
-                children: [
-                    '/html/tags',
-                    '/html/test',
-                ]
-            },
-        ]
+        nav,
+        sidebar,
     }
 }
