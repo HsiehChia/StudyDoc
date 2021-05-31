0efe474c1341137671412b2bcdd91c4a9b9e5f40
Jane <jane.xie@bundleb2b.net>
2021-05-31 15:25:34 +0800

    feat:complete bog's nav and sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,33 @@
 module.exports = {
     title: "HsiehChia's blog",
-    description: 'just write something funny'
+    description: "Jane's personal blog",
+    themeConfig: {
+        logo: '/assets/logo.png',
+        nav: [
+            {text: '首页', link: '/'},
+            {text: '技术分类', link: '/cate/'},
+            {text: '联系站长', link: '/contact/'},
+        ],
+        sidebar: [
+            {
+                title: '技术分类目录',
+                path: '/cate/',
+                collapsable: true,
+                sidebarDepth: 1,
+                children: [
+                  ['/cate/category', '目录结构'],
+                ]
+            },
+            {
+                title: 'HTML',
+                path: '/html/',
+                collapsable: true,
+                sidebarDepth: 1,
+                children: [
+                    '/html/tags',
+                    '/html/test',
+                ]
+            },
+        ]
+    }
 }
